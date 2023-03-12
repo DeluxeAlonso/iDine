4de3574486d987aec376c0cde9bf7c89f7595be1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-03-12 14:49:59 -0500

    Polishing designs with fonts and colors

--- iDine/ItemRowView.swift
+++ iDine/ItemRowView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ItemRowView: View {
     let item: MenuItem
 
+    private let restrictionColors: [String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green
+    ]
+
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2.0))
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }
+            Spacer()
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5.0)
+                    .background(restrictionColors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
