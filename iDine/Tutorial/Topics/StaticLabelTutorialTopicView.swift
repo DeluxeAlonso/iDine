afc62fcff6f32df423baad816fae60441eb98137
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-04-14 18:14:51 -0500

    How to create static labels with a Text view

--- iDine/Tutorial/Topics/StaticLabelTutorialTopicView.swift
+++ iDine/Tutorial/Topics/StaticLabelTutorialTopicView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct StaticLabelTutorialTopicView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Spacer()
+            Text("This is some longer text that is limited to three lines maximum, so anything more than that will cause the text to clip.")
+                .lineLimit(3)
+                .frame(width: 200)
+            Spacer()
+            Text("This is some longer text that is limited to a specific range of lines, so anything more than six lines will cause the text to clip.")
+                .lineLimit(3...6)
+                .frame(width: 200)
+            Spacer()
+            Text("This is always two lines")
+                .lineLimit(2, reservesSpace: true)
+            Spacer()
+            Text("This is an extremely long string of text that will never fit even the widest of iOS devices even if the user has their Dynamic Type setting as small as is possible, so in theory it should definitely demonstrate truncationMode().")
+                .lineLimit(1)
+                .truncationMode(.middle)
+            Spacer()
+        }
     }
 }
 
